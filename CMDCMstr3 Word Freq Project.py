7bb9482390473c517522018ad5d34b926af7d9db
SamR <79996273+CMDCMstr3@users.noreply.github.com>
2021-06-22 12:27:31 -0700

    uploaded 333 project files.

---
+++ CMDCMstr3 Word Freq Project.py
@@ -0,0 +1,30 @@
+import string
+# Opens and reads text file.
+text = open("PythonMission.txt", "r") 
+# This will create the dictionary that will be used.
+d = dict() 
+# This creates a loop through PythonMission.txt
+for line in text:
+# Changes all words in text to lower case.	
+	line = line.lower()
+# Removes all punctualation. Note: It took me a while to figure this out.	
+	line = line.translate(line.maketrans("", "", string.punctuation))
+	
+	words = line.split(" ")
+# starts an iteration through each word.
+for word in words:
+	
+	if word in d:
+# Counts each word by one incrementally.		
+		d[word] = d[word] + 1
+	
+	else:
+# Those words that appear once is assigned a 1. 	
+		d[word] = 1
+# Printing format for words in the dictionary.
+for key in list(d.keys()):
+	
+	print(key + ": ", d[key], ', ', sep='', end='')
+	
+	 
+
